fd8865fd0fa251d0203dfd78a196b4c6e1921dfa
kychongdev <kychongdev@gmail.com>
2023-12-17 02:12:57 +0800

    change to kickstart nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,3 @@
 vim.g.mapleader = " "
 vim.opt.number = true
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
-require('plugin.packer')
-require('plugin.lspconfig')
-require('plugin.telescope')
-require('plugin.bufferline')
-require('plugin.nvimtree') 
+
