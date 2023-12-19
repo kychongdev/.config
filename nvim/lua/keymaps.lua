693123c3de022eaf3b47833ee70ad0720f720b9c
kychongdev <kychongdev@gmail.com>
2023-12-20 01:34:13 +0800

    add keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -3,6 +3,8 @@
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set('i', 'jk', '<Esc>', {})
+vim.keymap.set('n', '<leader>:', vim.cmd.Ex)
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
