9622ac6908969589444d2cde5f4b65fc2e5624de
kychongdev <kychongdev@gmail.com>
2024-01-25 21:55:12 +0800

    add html and css to prettier format

--- nvim/lua/kychongdev/conform.lua
+++ nvim/lua/kychongdev/conform.lua
@@ -1,7 +1,13 @@
 require("conform").setup({
   formatters_by_ft = {
     -- Use a sub-list to run only the first available formatter
-    javascript = { { "prettierd", "prettier" } },
+    javascript = { "prettier" },
+    html = { "prettier" },
+    css = { "prettier" },
+    ["*.tsx"] = { "prettier" },
+    ["*.ts"] = { "prettier" }
+    ["*.jsx"] = { "prettier" },
+    ["*.js"] = { "prettier" }
   },
   format_on_save = {
     -- I recommend these options. See :help conform.format for details.
