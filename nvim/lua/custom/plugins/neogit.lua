7ed029405ab5012e61b92028eaa0ea99b6a975dd
kychongdev <kychongdev@gmail.com>
2024-03-17 20:12:08 +0800

    add neogit

---
+++ nvim/lua/custom/plugins/neogit.lua
@@ -0,0 +1,20 @@
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+
+    -- Only one of these is needed, not both.
+    'nvim-telescope/telescope.nvim', -- optional
+  },
+  config = true,
+  keys = {
+    {
+      '<leader>g',
+      function()
+        require('neogit').open()
+      end,
+      desc = 'Open Neo[G]it',
+    },
+  },
+}
