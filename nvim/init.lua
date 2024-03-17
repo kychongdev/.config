cf44e46a1f07656b20518ffcbe8f21eb3026f914
kychongdev <kychongdev@gmail.com>
2024-03-17 18:53:33 +0800

    Disable swap file

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,6 +154,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Disable swap file
+vim.opt.swapfile = false
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
