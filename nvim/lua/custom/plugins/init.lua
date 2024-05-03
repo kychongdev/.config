70bcf4842bc98bc8786a8d7ee1548d431ecfa62d
kychongdev <kychongdev@gmail.com>
2024-05-03 17:54:01 +0800

    Update

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -39,7 +39,7 @@ return {
         desc = '[A]ppend to harpoon',
       },
       {
-        '<leader>h',
+        '<leader>l',
         function()
           local harpoon = require 'harpoon'
           harpoon.ui:toggle_quick_menu(harpoon:list())
@@ -73,6 +73,74 @@ return {
       vim.api.nvim_set_hl(0, 'FloatBorder', { link = 'Normal' })
       vim.api.nvim_set_hl(0, 'LspInfoBorder', { link = 'Normal' })
       vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
+    end,
+  },
+  {
+    'RaafatTurki/corn.nvim',
+    opts = {
+      -- enables plugin auto commands
+      auto_cmds = true,
+
+      -- sorts diagnostics according to a criteria. must be one of `severity`, `severity_reverse`, `column`, `column_reverse`, `line_number` or `line_number_reverse`
+      sort_method = 'severity',
+
+      -- sets the scope to be searched for diagnostics, must be one of `line` or `file`
+      scope = 'line',
+
+      -- sets the style of the border, must be one of `single`, `double`, `rounded`, `solid`, `shadow` or `none`
+      border_style = 'single',
+
+      -- sets which vim modes corn isn't allowed to render in, should contain strings like 'n', 'i', 'v', 'V' .. etc
+      blacklisted_modes = {},
+
+      -- sets which severity corn isn't allowed to render in, should contain diagnostic severities like:
+      -- vim.diagnostic.severity.HINT
+      -- vim.diagnostic.severity.INFO
+      -- vim.diagnostic.severity.WARN
+      -- vim.diagnostic.severity.ERROR
+      blacklisted_severities = {},
+
+      -- highlights to use for each diagnostic severity level
+      highlights = {
+        error = 'DiagnosticFloatingError',
+        warn = 'DiagnosticFloatingWarn',
+        info = 'DiagnosticFloatingInfo',
+        hint = 'DiagnosticFloatingHint',
+      },
+
+      -- icons to use for each diagnostic severity level
+      icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I',
+      },
+
+      -- a preprocessor function that takes a raw Corn.Item and returns it after modification, could be used for truncation or other purposes
+      item_preprocess_func = function(item)
+        -- the default truncation logic is here ...
+        return item
+      end,
+
+      -- a hook that executes each time corn is toggled. the current state is provided via `is_hidden`
+      on_toggle = function(is_hidden)
+        -- custom logic goes here
+      end,
+    },
+  },
+  {
+    'wojciech-kulik/xcodebuild.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'MunifTanjim/nui.nvim',
+      -- 'nvim-tree/nvim-tree.lua', -- (optional) to manage project files
+      -- 'stevearc/oil.nvim', -- (optional) to manage project files
+      'nvim-treesitter/nvim-treesitter', -- (optional) for Quick tests support (required Swift parser)
+    },
+    config = function()
+      require('xcodebuild').setup {
+        -- put some options here or leave it empty to use default settings
+      }
     end,
   },
 }
