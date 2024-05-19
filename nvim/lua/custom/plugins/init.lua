a9265670921f6f158890bc3074e6df4b3bff0a78
kychongdev <kychongdev@gmail.com>
2024-05-19 14:18:13 +0800

    Remove neogit, add indent line, update package

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -4,26 +4,6 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
-
-      -- Only one of these is needed, not both.
-      'nvim-telescope/telescope.nvim', -- optional
-    },
-    config = true,
-    keys = {
-      {
-        '<leader>g',
-        function()
-          require('neogit').open()
-        end,
-        desc = 'Open Neo[G]it',
-      },
-    },
-  },
-  {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
