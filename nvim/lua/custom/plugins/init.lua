00ae008929796d1c39fb9b3a4ca191ef9dcf771a
kychongdev <kychongdev@gmail.com>
2025-04-27 18:55:10 +0800

    format theme

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -46,16 +46,17 @@ return {
   -- },
   {
     'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000,
     config = function()
       vim.o.background = 'dark' -- or "light" for light mode
-      vim.cmd "let g:gruxbox_material_foreground= 'mix'"
-      vim.cmd "let g:gruvbox_material_background= 'hard'"
-      vim.cmd 'let g:gruvbox_material_diagnostic_line_highlight=1'
-      vim.cmd "let g:gruvbox_material_diagnostic_virtual_text='colored'"
-      vim.cmd 'let g:gruvbox_material_enable_bold=1'
-      vim.cmd 'let g:gruvbox_material_enable_italic=1'
-      vim.cmd 'let g:gruvbox_material_dim_inactive_windows=1'
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_foreground = 'original'
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_diagnostic_line_highlight = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+      vim.g.gruvbox_material_dim_inactive_windows = 1
       vim.cmd [[colorscheme gruvbox-material]] -- Set color scheme
       -- changing bg and border colors
       vim.api.nvim_set_hl(0, 'FloatBorder', { link = 'Normal' })
