5a0f522aa0472d684147d3ac2b5f7b59777af169
kychongdev <kychongdev@gmail.com>
2025-05-12 00:04:06 +0800

    update nvim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -52,7 +52,7 @@ return {
       vim.o.background = 'dark' -- or "light" for light mode
       vim.g.gruvbox_material_enable_italic = 1
       vim.g.gruvbox_material_enable_bold = 1
-      vim.g.gruvbox_material_foreground = 'original'
+      vim.g.gruvbox_material_foreground = 'mix'
       vim.g.gruvbox_material_background = 'hard'
       vim.g.gruvbox_material_diagnostic_line_highlight = 1
       vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
